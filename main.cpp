297ee474ec43823e3ac89e4c127354fef28929d0
eyvan1 <ivanjheddumolong@gmail.com>
2025-02-10 00:23:25 +0000

    sayang 10points

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include<iostream>
-using namespace std;
-int main(){
-for (int i=1; i<=10; ++i)  { 
-    cout<<i<<endl;}
-    return 0;}
 
-    
+
+
+
+}
---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int num1, num2, num3, sum;
+
+ cout<<"ENTER A NUMBER: "<<endl;
+ cin>>num1;
+
+ cout<<"ENTER ANOTHER: "<<endl;
+ cin>>num2;
+
+ cout<<"ENTER ANOTHER: "<<endl;
+ cin>>num3;
+
+ sum=num1+num2+num3;
+
+ cout<<"THE SUM IS:"<<sum<<"\n\n"<<endl;
+ return 0;
+}
